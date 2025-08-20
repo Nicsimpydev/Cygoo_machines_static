3f85115315160532e79ec277a1fca33aa57af5df
simpydev <simpydev@yandex.com>
2025-08-20 15:17:26 +0200

    2nd

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Sélectionne la modale et les éléments nécessaires
+    const modal = document.getElementById("imageModal");
+    const modalImg = document.getElementById("modalImage");
+    const span = document.getElementsByClassName("close")[0];
+
+    // Gère l'ouverture de la modale au clic sur une image
+    const images = document.querySelectorAll('.gallery img');
+    images.forEach(img => {
+        img.addEventListener('click', function() {
+            modal.style.display = "block";
+            modalImg.src = this.dataset.full;
+        });
+    });
+
+    // Gère la fermeture de la modale au clic sur le bouton 'x'
+    span.onclick = function() { 
+        modal.style.display = "none";
+    }
+
+    // Gère la fermeture de la modale au clic en dehors de l'image
+    modal.onclick = function(event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    }
+});
